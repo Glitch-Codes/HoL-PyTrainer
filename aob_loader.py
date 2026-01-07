7ed912825ce5cde5b3e8e5966abb08d0351e492a
glitch-codes <benj0670@gmail.com>
2026-01-07 11:10:22 -0330

    Removed AOB loader file - redundant function of Utility

--- aob_loader.py
+++
@@ -1,8 +0,0 @@
-import utility
-
-def loadAOB(process, pattern, executible):
-    """Utility function to perform AOB scan and return address"""
-    addr = utility.aobScan(process, pattern, executible)
-    if not addr:
-        return None
-    return addr
